96dbe74509866e5d1798579bc1063eae791d0a0e
Dync_mark <chenpeng@dync.cc>
2019-05-14 12:04:19 +0800

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    APP_ID: 'anyrtcXJfqnOdWy8Di',
-    APP_TOKEN: '007ac828e3ca883f6bab728018eec068',
-    RTC_SERVER_URL: 'pro.anyrtc.io'
+    APP_ID: '',
+    APP_TOKEN: '',
+    RTC_SERVER_URL: ''
 };
